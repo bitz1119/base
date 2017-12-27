46abcd74abac553aec291f620959e8b5cee0cc7c
bittoo aggarwal <bitz1119@gmail.com>
2017-12-27 10:50:44 +0530

    class 1

---
+++ class 1/hello.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+// #define 3.14 pie
+using namespace std;
+main()
+{
+	//printing welcome message
+	// cout<<"welcome to waw";
+	// return 0;
+
+	int a,b;
+	cin>>a;
+	cin>>b;
+	int sum;
+	sum = a+b;
+	cout<<sum;
+	return 0;
+
+
+}
